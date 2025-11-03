7df8efbf5744965169fed2bff974c7ded810c501
Glauco <g.ant.facul@gmail.com>
2025-11-03 13:51:57 -0300

    sprite + move

--- main.cpp
+++ main.cpp
@@ -1,25 +1,47 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800,600}), "RPG");
-    sf::CircleShape shape(50.0f, 8);
-    shape.setFillColor(sf::Color::Blue);
-    shape.setPosition(sf::Vector2f(100,100));
-    shape.setOutlineThickness(1);
     
+    sf::Texture playerTexture; 
+    sf::Sprite playerSprite(playerTexture);
+    playerSprite.scale(sf::Vector2f(3, 3));
+    float playerSpd= 0.03;
+
+    if(!playerTexture.loadFromFile("assets/player/texture/playersheet.png")){
+        std::cout << "failed to load";
+    }
+
+    int xIndex= 0;
+    int yIndex= 0;
+    playerSprite.setTextureRect(sf::IntRect(sf::Vector2i(xIndex*16,yIndex*16), sf::Vector2i(16, 16)));
+
     while(window.isOpen()){
 //------------------------------UPDATE-------------------------------
-       while(const std::optional event= window.pollEvent()){
+        while(const std::optional event= window.pollEvent()){
             if(event->is<sf::Event::Closed>())
                 window.close();
         }
 //------------------------------UPDATE-------------------------------
-       
-
+        sf::Vector2f moveDir(0.0f, 0.0f);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)){
+            moveDir.y -= playerSpd;
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)){
+            moveDir.y += playerSpd;
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)){
+            moveDir.x -= playerSpd;
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)){
+            moveDir.x += playerSpd;
+        }
+        playerSprite.move(moveDir);
 //------------------------------DRAW-------------------------------
        window.clear();
-       window.draw(shape);
+       window.draw(playerSprite);
        window.display();
     }
 //------------------------------DRAW-------------------------------
